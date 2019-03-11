3b2270b7bd5faf3253abd34eea7a8e0753f19736
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2019-03-10 22:17:30 -0400

    Quick package updates

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import { CurrencyCalculatorContainer } from './components/';
 import './components/currencyCalculator.scss';
 
-
 ReactDOM.render(<CurrencyCalculatorContainer />, document.getElementById('root'));
+
+// Change the browser tab title
+document.title = 'Currency Calculator';
